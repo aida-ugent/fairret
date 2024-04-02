dfea5481b96e740884be6e2bb4220a0d53217995
Maarten Buyl <maarten.buyl@ugent.be>
2024-04-02 10:38:34 +0200

    Refactoring of statistic as a module

---
+++ fairret/statistic/__init__.py
@@ -0,0 +1,11 @@
+from .base import Statistic
+from .linear_fractional import (
+    LinearFractionalStatistic,
+    PositiveRate,
+    TruePositiveRate,
+    FalsePositiveRate,
+    PositivePredictiveValue,
+    FalseOmissionRate,
+    Accuracy,
+    FalseNegativeFalsePositiveFraction
+)
