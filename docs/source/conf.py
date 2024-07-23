5024c901071ed5910b7b605f1d29869fa92970cd
Maarten Buyl <maarten.buyl@ugent.be>
2024-07-23 18:51:49 +0200

    Documentation improvements

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -20,8 +20,7 @@ extensions = [
     'sphinx.ext.autodoc',
     'sphinx.ext.autosummary',
     'sphinx.ext.napoleon',
-    'nbsphinx',
-    "nbsphinx_link",
+    'nbsphinx'
 ]
 
 templates_path = ['_templates']
